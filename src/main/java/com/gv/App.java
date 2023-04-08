38875fce049dd0c65210466991ad6ddc46cfef3d
vicente322 <vicentehofmeister@gmail.com>
2023-04-08 17:12:22 -0300

    Avanco App

--- src/main/java/com/gv/App.java
+++ src/main/java/com/gv/App.java
@@ -1,5 +1,27 @@
 package com.gv;
 
+import java.util.Scanner;
+
 public class App {
+
+      public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            String cep;
+
+            ConsultaCep cc = new ConsultaCep();
+
+            System.out.println("***CONSULTA DE CEP***");
+            System.out.println("Insira o CEP desejado ou aperte enter para pesquisar por rua");
+            System.out.println("CEP: ");
+            cep = sc.nextLine();
+            
+            if (cep.length() != 0){
+                  MontaPesquisa pesquisa = new MontaPesquisa();
+                  pesquisa.setCep(cep);
+                  ConsultaCep consulta = pesquisa.consultaCEP();
+            }
+      }
       
+
+
 }
